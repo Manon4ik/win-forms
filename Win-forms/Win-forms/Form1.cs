c4a0a2307de90a35f4bcf470b463a406992948f4
Manon4ik <manon4ik@gmail.com>
2016-02-02 17:19:15 +0200

    Счетчик готов

--- Win-forms/Win-forms/Form1.cs
+++ Win-forms/Win-forms/Form1.cs
@@ -12,6 +12,9 @@ namespace Win_forms
 {
     public partial class MainForm : Form
     {
+
+        int count = 0;
+
         public MainForm()
         {
             InitializeComponent();
@@ -25,6 +28,24 @@ namespace Win_forms
         private void tsmiAbout_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Информация о программе.", "О программе");
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            count++;
+            lblCount.Text = count.ToString();
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            count--;
+            lblCount.Text = count.ToString();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            lblCount.Text = Convert.ToString(count);
         }
     }
 }
